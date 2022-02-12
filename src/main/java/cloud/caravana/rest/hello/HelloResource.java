4e6d9edea7de730d18b00a70e1d6fb0eb224b10e
Julio Faerman <jfaerman@gmail.com>
2022-02-12 14:13:12 +0100

    adding database

--- src/main/java/cloud/caravana/rest/hello/HelloResource.java
+++ src/main/java/cloud/caravana/rest/hello/HelloResource.java
@@ -1,12 +1,20 @@
 package cloud.caravana.rest.hello;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import cloud.caravana.algo.reverse.ReverseInplace;
+import io.vertx.core.json.JsonObject;
 
 @Path("/hello")
 public class HelloResource {
     @GET
-    public String hello() {
-        return "Hello World!";
+    public JsonObject hello(@QueryParam("name") @DefaultValue("fulano") String name) {
+        var reverse = ReverseInplace.reverse(name);
+        var json = new JsonObject();
+        json.put("message", "Hello " + reverse);
+        return json;
     }
 }
